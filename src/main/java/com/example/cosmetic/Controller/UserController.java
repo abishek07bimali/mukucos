896be165a7d97f24dbb6a1b0c51ed8d2f263c427
Shadow2078 <karkisuman2021@gmail.com>
2023-02-08 17:35:31 +0545

    gonna push

--- src/main/java/com/example/cosmetic/Controller/UserController.java
+++ src/main/java/com/example/cosmetic/Controller/UserController.java
@@ -3,16 +3,20 @@ package com.example.cosmetic.Controller;
 
 
 import com.example.cosmetic.Pojo.UserPojo;
+import com.example.cosmetic.Services.UserServices;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/user")
 public class UserController {
+    private final UserServices userService;
     @GetMapping("/homepage") //first maa chaine kura
     public String getSetting(Model model) {
         return "index";
@@ -22,6 +26,11 @@ public class UserController {
     public String getSignupPage(Model model) {
         model.addAttribute("create", new UserPojo());
         return "signin";
+    }
+    @PostMapping("/saveuser")
+    public String saveUser(@Valid UserPojo userPojo) {
+        userService.save(userPojo);
+        return "redirect:/signup";
     }
 }
 
